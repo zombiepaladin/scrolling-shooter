ec989e2326c5c5742dd0e285cc24cd4e3832a6f5
Nathan Bean <nhb7817@ksu.edu>
2012-08-29 19:05:18 -0500

    added GameObject.cs

---
+++ ScrollingShooter/ScrollingShooter/GameObject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ScrollingShooter
+{
+    /// <summary>
+    /// The base class for all game object
+    /// </summary>
+    public abstract class GameObject
+    {
+        /// <summary>
+        /// The bounds of this game object in the game world
+        /// </summary>
+        public abstract Rectangle Bounds { get; }
+
+        /// <summary>
+        /// Updates the game object - called every frame
+        /// </summary>
+        /// <param name="elaspsedTime"></param>
+        public abstract void Update(float elaspsedTime);
+
+        /// <summary>
+        /// Renders the game object - called every frame
+        /// </summary>
+        /// <param name="elaspedTime"></param>
+        /// <param name="spriteBatch"></param>
+        public abstract void Draw(float elaspedTime, SpriteBatch spriteBatch);
+    }
+}

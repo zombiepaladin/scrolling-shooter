88cc509e2e9bb3dc0f4108096a5151e096462dd5
Versaiteis <nickaboen+GitHub@gmail.com>
2012-09-01 14:38:44 -0500

    Added a file

---
+++ ScrollingShooter/ScrollingShooter/Powerups/HomingMissilesPowerup.cs
@@ -0,0 +1,24 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace ScrollingShooter
+{
+    /// <summary>
+    /// A class representing a fireball powerup
+    /// </summary>
+    public class FireballPowerup : Powerup
+    {
+        /// <summary>
+        /// Creates a new fireball powerup
+        /// </summary>
+        /// <param name="contentManager">A ContentManager to load resources with</param>
+        /// <param name="position">The position the fireball powerup in the world</param>
+        public FireballPowerup(ContentManager contentManager, Vector2 position)
+        {
+            this.spriteSource = new Rectangle(48, 114, 23, 23);
+            this.spriteSheet = contentManager.Load<Texture2D>("Spritesheets/tyrian.shp.010008");
+            this.spriteBounds = new Rectangle((int)position.X, (int)position.Y, 23, 23);
+        }
+    }
+}

425f2367927d894c298e74ea5455bee2746fc7c4
Devin Kelly-Collins <dkellycollins@gmail.com>
2012-09-12 20:20:30 -0800

    Fixed ToPlayerBullet.cs

--- ScrollingShooter/ScrollingShooter/Projectiles/ToPlayerBullet.cs
+++ ScrollingShooter/ScrollingShooter/Projectiles/ToPlayerBullet.cs
@@ -5,14 +5,22 @@ using System;
 
 namespace ScrollingShooter
 {
+    /// <summary>
+    /// A bullet that travels towards the player.
+    /// </summary>
     public class ToPlayerBullet : Projectile
     {
         //Constants
         private const string SPRITESHEET = "Spritesheets/tyrian.shp.01D8A7";
         private static readonly Rectangle SPRITEBOUNDS = new Rectangle(38, 57, 7, 11);
-        private const int X_VELOCITY = 300;
-        private const int Y_VELOCITY = 300;
+        private static readonly Vector2 VELOCITY = new Vector2(300);
 
+        /// <summary>
+        /// Creates a new bullet that will travel towards the player's current position.
+        /// </summary>
+        /// <param name="id">Id for the bullet.</param>
+        /// <param name="content">ContentManager to load content with.</param>
+        /// <param name="position">Starting position for the bullet.</param>
         public ToPlayerBullet(uint id, ContentManager content, Vector2 position)
             : base(id)
         {
@@ -22,21 +30,12 @@ namespace ScrollingShooter
 
             this.position = position;
 
-            this.velocity = toPlayerVelocity();
-        }
-
-        /// <summary>
-        /// Creates a velocity that is in the direction of the player.
-        /// </summary>
-        /// <returns>A new velocity</returns>
-        private Vector2 toPlayerVelocity()
-        {
-            //Don't want to deal with trig. Just fire in one of eight directions.
-            //Will add trig functions soon.
+            //Fire at the player.
             PlayerShip player = ScrollingShooterGame.Game.player;
-            Vector2 positionVector = position - player.GetPosition();
+            Vector2 positionVector = player.GetPosition() - position;
+            positionVector.Normalize();
 
-            return new Vector2(X_VELOCITY * -Math.Sign(positionVector.X), Y_VELOCITY * -Math.Sign(positionVector.Y));
+            this.velocity = positionVector * VELOCITY;
         }
     }
 }

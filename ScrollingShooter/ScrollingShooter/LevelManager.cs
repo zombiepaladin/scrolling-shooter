e9fa521f8d6e8c54c2041dff4cf600003a4aebfd
Nathan Bean <nhb7817@ksu.edu>
2012-09-19 10:27:02 -0500

    Added level manager

---
+++ ScrollingShooter/ScrollingShooter/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ScrollingShooterWindowsLibrary;
+
+namespace ScrollingShooter
+{
+    public class LevelManager
+    {
+        Viewport worldViewport;
+
+        public Tilemap CurrentLevel;
+
+
+        public void LoadLevel(string level)
+        {
+
+
+        }
+
+        public void Draw(float elapsedTime, SpriteBatch spriteBatch)
+        {
+ 
+            
+        }
+    }
+}
